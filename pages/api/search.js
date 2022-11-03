f2eb638897297713279992456710ee0d1da7ebe4
KalyanReddy-K <2215316244@gitam.in>
2022-11-02 20:38:12 -0500

    sorted list

--- pages/api/search.js
+++ pages/api/search.js
@@ -5,8 +5,13 @@ import Rate from "../../models/ratesModel";
  * @param {import('next').NextApiRequest} req
  * @param {import('next').NextApiResponse} res
  */
+
+
+ 
+
 export default async function search(req, res) {
   try {
+    let sorted_rates
     const { current, destination, date } = req.body
 
     console.log('CONNECTING TO MONGO');
@@ -14,10 +19,9 @@ export default async function search(req, res) {
     console.log('CONNECTED TO MONGO');
 
     const sample_rates = await Rate.findOne({from: current,to: destination,date: date});
-
+    sorted_rates = sample_rates?Object.fromEntries(Object.entries(sample_rates["rates"]).sort((a,b)=>a[1]-b[1])):{}
     console.log('FETCHED');
-    console.log(sample_rates)
-    sample_rates?res.json(sample_rates["rates"]): res.status(500).send('No Result')
+    sorted_rates?res.json(sorted_rates): res.status(500).send('No Result')
   } catch (error) {
     console.log(error);
     res.status(500).send(error)
